f559c2d66cce3274ab7486ddcfef51ec9759676f
UHwolf <dakounamr6@gmail.com>
2026-04-13 02:45:14 +0200

    google verification file

--- server.js
+++ server.js
@@ -2,6 +2,19 @@
 const path = require("path");
 const dotenv = require('dotenv');
 
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0");
+
 // Buscamos el archivo .env específicamente en la carpeta donde está este server.js
 const result = dotenv.config({ path: path.join(__dirname, '.env') });
 
